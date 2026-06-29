bc8b45c76d4c2d45b03c83d1395506073ba10a48
SyedaMumtaz34 <az2209107@gmail.com>
2026-06-29 21:19:44 +0500

    new file:   lesson-3/after-class-project.py

---
+++ lesson-3/after-class-project.py
@@ -0,0 +1,9 @@
+# Take input from the user
+message = input("Enter the message: ")
+
+# Convert to uppercase
+upper_message = message.upper()
+
+# Print the result
+print("Congratulations message in uppercase:")
+print(upper_message)
