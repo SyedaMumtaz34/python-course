03f5d9ba8ee3f3bde478e9327e5eb8413cd405c7
SyedaMumtaz34 <az2209107@gmail.com>
2026-07-01 22:26:57 +0500

    modified:   lesson-5/after-class-project.py 	new file:   lesson-6/after-class-project.py

---
+++ lesson-5/after-class-project.py
@@ -0,0 +1,6 @@
+temperature = int(input("Enter the temperature in °C: "))
+
+if temperature >= 30:
+    print("It is summer. Wear light cotton clothes.")
+else:
+    print("It is not very hot. Normal clothes are fine.")
---
+++ lesson-6/after-class-project.py
@@ -0,0 +1,7 @@
+alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ch = input("Enter a character: ")
+
+if ch in alphabets:
+    print("Yes, it is a character")
+else:
+    print("It is not a character")
