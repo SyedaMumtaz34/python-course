6a709894d669107f3dfdfd3159c34eb79c4ac4f1
SyedaMumtaz34 <az2209107@gmail.com>
2026-07-11 13:03:25 +0500

    modified:   lesson-6 module-2/Activity1.py

--- lesson-6 module-2/Activity1.py
+++ lesson-6 module-2/Activity1.py
@@ -2,13 +2,13 @@ print("welcome to number guesseing game ")
 secret_number=27
 
 guess=int(input("Enter a number "))
-if guess is (1,50) :
+if guess == (1,50) :
     print("ice cold congratulations you have passed ")
-    if guess is (40,50):
+    if guess == (40,50):
         print("cold you are close ")
-        if guess is(30,40):
+        if guess ==(30,40):
             print("warm you are far ")
-            if guess is(20,30):
+            if guess ==(20,30):
                 print("hot you are too far ")
             
 else:
