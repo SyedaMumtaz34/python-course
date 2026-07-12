399a8019f66818abc995e874341fb596491ec565
SyedaMumtaz34 <az2209107@gmail.com>
2026-07-12 21:49:17 +0500

    modified:   lesson4-module2/after-class-project.py

--- lesson4-module2/after-class-project.py
+++ lesson4-module2/after-class-project.py
@@ -1,51 +1,22 @@
-print("===== Inventory Stock Counter =====")
+products = int(input("Enter number of products: "))
+boxes = int(input("Enter the number of boxes: "))
+stock = []
+Product = 1
 
-box_sizes = [20, 10, 5, 1]
+while Product <= products:
+    print("Product", Product)
+    box = 1
+    product_stock = []
+    while box <= boxes:
+        quantity = int(input("Enter the quantity in box: "))
+        product_stock.append(quantity)
+        print("Box", box, "contains", quantity, "items")
+        box += 1
+    stock.append(product_stock)
+    Product += 1
 
-products_counted = 0
-total_items = 0
-log = []
-
-num_products = int(input("Enter number of products: "))
-
-while products_counted < num_products:
-
-    product = input("\nEnter product name: ")
-    quantity = int(input("Enter quantity: "))
-
-    if quantity <= 0:
-        print("Invalid quantity!")
-        continue
-
-    total_items += quantity
-    remaining = quantity
-    breakdown = {}
-
-    i = 0
-    while i < len(box_sizes):
-        size = box_sizes[i]
-        boxes = remaining // size
-        remaining = remaining % size
-        breakdown[size] = boxes
-        i += 1
-
-    log.append((product, breakdown))
-    products_counted += 1
-
-print("\n===== Product Breakdown =====")
-
-for item in log:
-    print("\nProduct:", item[0])
-    for size in box_sizes:
-        print("Box", size, ":", item[1][size])
-
-print("\n===== Final Report =====")
-
-for size in box_sizes:
-    total_boxes = 0
-    for item in log:
-        total_boxes += item[1][size]
-    print("Box", size, "used:", total_boxes)
-
-print("\nTotal Products:", products_counted)
-print("Total Items:", total_items)
+print("====Final stock report====")
+for product in range(products):
+    print("Product", product+1)
+    for box in range(boxes):
+        print("Box", box+1, "contains", stock[product][box], "items")
