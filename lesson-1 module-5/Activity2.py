85f7cfaf7da28b182adfb7406435b091838dca27
SyedaMumtaz34 <az2209107@gmail.com>
2026-07-28 12:07:00 +0500

    new file:   lesson-1 module-5/Activity1.py 	new file:   lesson-1 module-5/Activity2.py 	new file:   lesson-1 module-5/Activity3.py

---
+++ lesson-1 module-5/Activity2.py
@@ -0,0 +1,8 @@
+class vehical:
+    def __init__(self,max_speed,mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+ob=vehical(240,18)
+print("model maximum speed: ",ob.max_speed)
+print("model mileage: ",ob.mileage)
+        
---
+++ lesson-1 module-5/Activity3.py
@@ -0,0 +1,11 @@
+class parrot:
+    species="bird"
+    def __init__(self,name,age ):
+        self.name=name
+        self.age=age
+blu=parrot("blu",10)
+woo=parrot("woo",15)
+print("blu is a ",blu.species)
+print("woo is a ",woo.species)
+print("{} is {} years old ".format(blu.name,blu.age))
+print("{} is {} years old ".format(woo.name,woo.age))
