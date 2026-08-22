680787c5c52838068e316e5eddb40ac47e5ebdcc
SyedaMumtaz34 <az2209107@gmail.com>
2026-08-22 19:17:07 +0500

    new file:   lesson-5 module-6/me.jpg 	new file:   lesson-5 module-7/Activity1.py 	new file:   lesson-5 module-7/Activity2.py 	new file:   lesson-5 module-7/app_img.jpg

---
+++ lesson-5 module-7/Activity1.py
@@ -0,0 +1,16 @@
+from tkinter import *
+root=Tk()
+root.geometry("400x300")
+root.title("main")
+def topwin():
+    top=Toplevel()
+    top.geometry("180x100")
+    top.title("Toplevel")
+    label=Label(top,text="this is a toplevel window ")
+    label.pack()
+    top.mainloop()
+label2=Label(root,text="this is a main window ")
+btn=Button(root,text="click here to open another window",command=topwin)
+label2.pack()
+btn.pack()
+root.mainloop()
